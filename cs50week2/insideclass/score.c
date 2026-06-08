23a262cabc2a95b3746781da9ae52f754fc17554
Srajal-Shrestha <srajalpradhan25@gmail.com>
2026-06-08 19:33:17 +0545

    score average

---
+++ cs50week2/insideclass/score.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int score1 = 76;
+    int score2 = 75;
+    int score3 = 56;
+    int score4 = 65;
+
+    printf("Average: %i\n", (score1 + score2 + score3 + score4) / 4);
+
+}
